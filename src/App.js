808381038e74d86e0c9bf73558149a61851fe1bf
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2020-02-19 12:08:55 +0700

    add fontawesome and bootstarap

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FontAwesome from "react-fontawesome";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
+export default class App extends Component {
+  constructor() {
+    super();
     this.state = {
       items: [],
-      input: '',
+      input: "",
       isEditing: false
-    }
+    };
   }
-  onEditing = () => { }
-  onChange = e => { }
-  onSubmit = e => { }
-  onClick = id => { }
-  onDelete = id => { }
+  onEditing = () => {};
+  onChange = e => {};
+  onSubmit = e => {};
+  onClick = id => {};
+  onDelete = id => {};
+
   render() {
     return (
       <form id="todo-list">
-         
-        { this.state.items.length < 0 ?
-        <span className="todo-wrap">
-          <span >
-            <input type="checkbox"  />
-            <label for="1" className="todo"><i className="fa fa-check"></i></label>
+        {this.state.items.length < 0 ? (
+          <span className="todo-wrap">
+            <span>
+              <input type="checkbox" />
+              <label for="1" className="todo">
+                <FontAwesome className="fa fa-check" />
+              </label>
+            </span>
+
+            <span className="delete-item" title="remove">
+              <FontAwesome className="fa fa-times-circle" />
+            </span>
           </span>
-       
-          <span className="delete-item" title="remove" >
-            <i className="fa fa-times-circle"></i>
-          </span> 
-        </span> : <p></p>
-        }
-        <span className="todo-wrap" ><input /></span>
-        <div id="add-todo"><i className="fa fa-plus"></i> &nbsp; Add an Item</div>
+        ) : (
+          <p></p>
+        )}
+        <span className="todo-wrap">
+          <input />
+        </span>
+        <div id="add-todo">
+          <FontAwesome className="fa fa-plus" /> &nbsp; Add an Item
+        </div>
       </form>
     );
   }
