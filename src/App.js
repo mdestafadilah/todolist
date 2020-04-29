d163ae91b4ac2f1f5e035871583744cdf5d545a1
Desta Muchamad <desta.muchamad@my.gavilan.edu>
2020-04-30 05:55:00 +0700

    fontawesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 import "./App.css";
-import FontAwesome from "react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faCheck, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default class App extends Component {
   constructor() {
@@ -9,55 +10,62 @@ export default class App extends Component {
     this.state = {
       items: [],
       input: "",
-      isEditing: false
+      isEditing: false,
     };
   }
   onEditing = () => {
     this.setState({ isEditing: true });
   };
-  onChange = e => {
+  onChange = (e) => {
     console.log(e.target.value);
     this.setState({ input: e.target.value });
   };
-  onSubmit = e => {
+  onSubmit = (e) => {
     e.preventDefault();
-    this.setState(state => ({
+    this.setState((state) => ({
       items: state.items.concat(this.state.input),
-      input: ""
+      input: "",
     }));
     console.log(this.state.input);
   };
-  onClick = id => {};
-  onDelete = id => {};
+  onClick = (id) => {};
+  onDelete = (id) => {};
 
   render() {
     return (
-      <form id="todo-list" onSubmit={e => this.onSubmit(e)}>
-        {this.state.items.length < 0 ? (
-          <span className="todo-wrap">
-            <span>
-              <input type="checkbox" />
-              <label for="1" className="todo">
-                <FontAwesome className="fa fa-check" />
-              </label>
-            </span>
+      <form id="todo-list" onSubmit={(e) => this.onSubmit(e)}>
+        {this.state.items.length > 0 ? (
+          this.state.items.map((item, key) => {
+            return (
+              <span className="todo-wrap" key={key}>
+                <span>
+                  <input type="checkbox" />
+                  <label htmlFor={key} className="todo">
+                    <FontAwesomeIcon icon={faCheck} /> {item}
+                  </label>
+                </span>
 
-            <span className="delete-item" title="remove">
-              <FontAwesome className="fa fa-times-circle" />
-            </span>
-          </span>
+                <span className="delete-item" title="remove">
+                  <FontAwesomeIcon icon={faCheck} />
+                </span>
+              </span>
+            );
+          })
         ) : (
           <p></p>
         )}
         {this.state.isEditing ? (
           <span className="todo-wrap">
-            <input value={this.state.input} onChange={e => this.onChange(e)} />
+            <input
+              value={this.state.input}
+              onChange={(e) => this.onChange(e)}
+            />
           </span>
         ) : (
           false
         )}
         {/* <div id="add-todo"> */}
-        <FontAwesome name="edit" className="fa fa-plus" /> &nbsp;
+        <FontAwesomeIcon icon={faPlus} /> &nbsp;
         <a id="add-todo" onClick={() => this.onEditing()}>
           Add an Item
         </a>
